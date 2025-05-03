399d6e5ab56d6c04d4503a83d8ab7bf8774bef1c
Charl Cronje <charlc@digitalcabinet.co.za>
2025-05-03 23:30:50 +0200

    Incldues commit

---
+++ backend/admin/includes/navbar.php
@@ -0,0 +1,18 @@
+<!-- backend/admin/includes/navbar.php -->
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="index.php">Media Share Admin</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="w-100"></div>
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap d-flex align-items-center">
+            <div class="dark-mode-toggle-container px-3">
+                <div class="dark-mode-toggle" title="Toggle Dark Mode">
+                    <i class="bi bi-sun-fill"></i>
+                </div>
+            </div>
+            <a class="nav-link px-3" href="logout.php">Sign out</a>
+        </div>
+    </div>
+</header>
