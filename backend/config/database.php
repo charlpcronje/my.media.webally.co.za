771eb9b5c4307cd0be0dbe35e7b9fa25d5d404f7
Charl Cronje <charlc@digitalcabinet.co.za>
2025-05-03 23:26:44 +0200

    Delete files from previou version

--- backend/config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Database configuration
-$db_host = 'localhost';
-$db_name = 'my_media';
-$db_user = 'cp';
-$db_pass = '4334.4334';
--- backend/database/admin_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, email, password, role) 
-VALUES ('admin', 'charl@webally.co.za', SHA2('4334.4334', 256), 'admin');
